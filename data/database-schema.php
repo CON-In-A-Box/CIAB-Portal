c0dfdae1cd2f3096f0afca04a9cc1c1e682108eb
Thomas Keeley <tskeeley@gmail.com>
2018-03-02 10:22:31 -0500

    Updated database schema to 2018030200, adding foreign key constraints

--- data/database-schema.php
+++ data/database-schema.php
@@ -5,7 +5,11 @@
 
 // The Current CIAB DB Schema in an array for checking and applying
 
-$DB_tables = [
+class SCHEMA
+{
+  public static $REQUIED_DB_SCHEMA = 2018030200; // Current DB Version - YYYYMMDDvv format (vv=daily counter form 00)
+
+  public static $DB_tables = [
     'Configuration' => [
         'Field' => 'VARCHAR(15) NOT NULL PRIMARY KEY',
         'Value' => 'VARCHAR(100) NOT NULL',
@@ -17,6 +21,9 @@ $DB_tables = [
         'PositionID' => 'INT UNSIGNED NOT NULL',
         'Note' => 'VARCHAR(100)',
         'YearID' => 'INT UNSIGNED NOT NULL',
+        'FOREIGN KEY (DepartmentID)' => 'REFERENCES Departments (DepartmentID) ON DELETE RESTRICT ON UPDATE CASCADE',
+        'FOREIGN KEY (PositionID)' => 'REFERENCES ConComPositions (PositionID) ON DELETE RESTRICT ON UPDATE CASCADE',
+        'FOREIGN KEY (YearID)' => 'REFERENCES ConventionYear (YearID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'ConComPositions' => [
         'PositionID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
@@ -30,34 +37,41 @@ $DB_tables = [
         'DepartmentID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
         'Name' => 'VARCHAR(50) NOT NULL',
         'ParentDepartmentID' => 'INT UNSIGNED NOT NULL',
+        'FOREIGN KEY (ParentDepartmentID)' => 'REFERENCES Departments (DepartmentID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'ElegibleVoters' => [
         'VoterRecordID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
         'AccountID' => 'INT UNSIGNED NOT NULL',
         'YearID' => 'INT UNSIGNED NOT NULL',
+        'FOREIGN KEY (YearID)' => 'REFERENCES ConventionYear (YearID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'EMails' => [
         'EMailAliasID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
         'DepartmentID' => 'INT UNSIGNED NOT NULL',
         'IsAlias' => 'BOOLEAN',
         'EMail' => 'VARCHAR(100) NOT NULL',
+        'FOREIGN KEY (DepartmentID)' => 'REFERENCES Departments (DepartmentID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'HourRedemptions' => [
         'ClaimID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
         'AccountID' => 'INT UNSIGNED NOT NULL',
         'PrizeID' => 'INT UNSIGNED NOT NULL',
         'YearID' => 'INT UNSIGNED NOT NULL',
+        'FOREIGN KEY (PrizeID)' => 'REFERENCES VolunteerRewards (PrizeID) ON DELETE RESTRICT ON UPDATE CASCADE',
+        'FOREIGN KEY (YearID)' => 'REFERENCES ConventionYear (YearID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'MeetingAttendance' => [
         'AttendanceRecordID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
         'AccountID' => 'INT UNSIGNED NOT NULL',
         'MeetingID' => 'INT UNSIGNED NOT NULL',
+        'FOREIGN KEY (MeetingID)' => 'REFERENCES OfficialMeetings (MeetingID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'OfficialMeetings' => [
         'MeetingID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
         'Name' => 'VARCHAR(50) NOT NULL',
         'Date' => 'DATE NOT NULL',
         'YearID' => 'INT UNSIGNED NOT NULL',
+        'FOREIGN KEY (YearID)' => 'REFERENCES ConventionYear (YearID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'RewardGroup' => [
         'RewardGroupID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
@@ -73,6 +87,8 @@ $DB_tables = [
         'EnteredByID' => 'INT UNSIGNED NOT NULL',
         'AuthorizedByID' => 'INT UNSIGNED NOT NULL',
         'YearID' => 'INT UNSIGNED NOT NULL',
+        'FOREIGN KEY (DepartmentID)' => 'REFERENCES Departments (DepartmentID) ON DELETE RESTRICT ON UPDATE CASCADE',
+        'FOREIGN KEY (YearID)' => 'REFERENCES ConventionYear (YearID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
     'VolunteerRewards' => [
         'PrizeID' => 'INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT',
@@ -81,6 +97,8 @@ $DB_tables = [
         'Promo' => 'BOOLEAN',
         'RewardGroupID' => 'INT UNSIGNED',
         'TotalInventory' => 'INT NOT NULL',
+        'FOREIGN KEY (RewardGroupID)' => 'REFERENCES RewardGroup (RewardGroupID) ON DELETE RESTRICT ON UPDATE CASCADE',
     ],
-]
+  ];
+}
 ?>
