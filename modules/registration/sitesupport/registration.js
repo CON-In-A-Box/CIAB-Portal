78f6300274069afa49b32c73fefb8f162cb1caef
Aric Stewart <aric@codeweavers.com>
2018-04-10 17:32:46 -0500

    move spinner to common code

--- modules/registration/sitesupport/registration.js
+++ modules/registration/sitesupport/registration.js
@@ -7,28 +7,6 @@
 
 'use strict';
 
-function showSpinner() {
-  var dlg = document.getElementById('refresh_spinner');
-  if (!dlg) {
-    dlg = document.createElement('DIV');
-    dlg.classList.add('w3-modal');
-    dlg.id = 'refresh_spinner';
-    var content = document.createElement('DIV');
-    dlg.appendChild(content);
-    content.classList.add('w3-modal-content');
-    content.classList.add('w3-jumbo');
-    content.classList.add('w3-center');
-    content.classList.add('w3-round-xxlarge');
-    content.style.width = '90px';
-    content.style.backgroundColor = 'orange';
-    var span = document.createElement('SPAN');
-    span.innerHTML = '<i class="fa fa-refresh w3-spin"></i>';
-    content.appendChild(span);
-    document.body.appendChild(dlg);
-  }
-  dlg.style.display = 'block';
-}
-
 function refreshBadgeData(badge) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
--- sitesupport/common.js
+++ sitesupport/common.js
@@ -18,3 +18,29 @@ function escapeHtml(text) {
 
   return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
+
+function showSpinner() {
+  var dlg = document.getElementById('refresh_spinner');
+  if (!dlg) {
+    dlg = document.createElement('DIV');
+    dlg.classList.add('w3-modal');
+    dlg.id = 'refresh_spinner';
+    var content = document.createElement('DIV');
+    dlg.appendChild(content);
+    content.classList.add('w3-modal-content');
+    content.classList.add('w3-jumbo');
+    content.classList.add('w3-center');
+    content.classList.add('w3-round-xxlarge');
+    content.style.width = '90px';
+    content.style.backgroundColor = 'orange';
+    var span = document.createElement('SPAN');
+    span.innerHTML = '<i class="fa fa-refresh w3-spin"></i>';
+    content.appendChild(span);
+    document.body.appendChild(dlg);
+  }
+  dlg.style.display = 'block';
+}
+
+function hideSpinner() {
+  document.getElementById('refresh_spinner').style.display = 'none';
+}
