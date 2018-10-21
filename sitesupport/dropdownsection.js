86e236390173ce1c2afc74bae78619ce429fd7a7
Aric Stewart <aric@codeweavers.com>
2018-10-21 09:38:26 -0500

    Allow dropdown sections to be identified by class and name
    This also allows a single dropdown to change the state of multiple sections

--- sitesupport/dropdownsection.js
+++ sitesupport/dropdownsection.js
@@ -6,14 +6,25 @@
 /* jshint -W097 */
 
 function expandSection(id) {
-  var x = document.getElementById(id);
+  var x = [document.getElementById(id)];
+  if (x[0] === null) {
+    x = document.getElementsByClassName(id);
+  }
+  if (x === null || x.length === 0) {
+    x = document.getElementsByName(id);
+  }
+  if (x === null || x.length === 0) {
+    return;
+  }
   var y = document.getElementById(id + '_arrow');
-  if (x.className.indexOf('w3-show') == -1) {
-    x.className += ' w3-show';
-    y.className = 'fa fa-caret-up';
-  } else {
-    x.className = x.className.replace(' w3-show', '');
-    y.className = 'fa fa-caret-down';
+  for (var i = 0; i < x.length; i++) {
+    if (x[i].className.indexOf('w3-show') == -1) {
+      x[i].className += ' w3-show';
+      y.className = 'fa fa-caret-up';
+    } else {
+      x[i].className = x[i].className.replace(' w3-show', '');
+      y.className = 'fa fa-caret-down';
+    }
   }
 
 }
