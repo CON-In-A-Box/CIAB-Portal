7b0a9d289709d5764b44282f0332fccdf41ab50f
Aric Stewart <aric@codeweavers.com>
2018-12-18 20:01:51 -0600

    Clean up db admin page a bit

---
+++ modules/admin/sitesupport/admin.js
@@ -0,0 +1,40 @@
+/* jshint browser: true */
+/* jshint -W097 */
+
+function downloadLog() {
+  window.location = 'index.php?Function=admin&downloadLog=db';
+
+}
+
+function setField(field) {
+  var value = document.getElementById('config_' + field).value;
+  value = btoa(value);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      location.reload();
+    }
+  };
+  xhttp.open('POST', 'index.php?Function=admin', true);
+  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhttp.send('&setField=' + field + '&value=' + value);
+
+}
+
+function addField() {
+  var field = document.getElementById('new_Field').value;
+  var value = document.getElementById('new_Value').value;
+  value = btoa(value);
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      location.reload();
+    }
+  };
+  xhttp.open('POST', 'index.php?Function=admin', true);
+  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhttp.send('&newField=' + field + '&value=' + value);
+
+}
+
