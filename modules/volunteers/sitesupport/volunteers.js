3b6a1e3086cc448f4544d7d1ed5d088b44ecee12
Aric Stewart <aric@codeweavers.com>
2018-12-21 22:17:15 -0600

    Move the kiosk mode out of the volunteer module
    Also move more of the inner working into its own set of methods.
    This will make it possible to add kiosk functionality to other
    areas of the code. The current vision has kisok functionality
    in a registration module as well.

--- modules/volunteers/sitesupport/volunteers.js
+++ modules/volunteers/sitesupport/volunteers.js
@@ -5,7 +5,7 @@
 /* jshint browser: true */
 /* jshint -W097 */
 /* globals confirmbox, userId, escapeHtml, userEmail, checkAuthentication,
-           kioskMode, groupData, checkAuthentication, adminMode, unclaimed,
+           groupData, checkAuthentication, adminMode, unclaimed,
            hoursRemain, userLookup */
 
 'use strict';
@@ -560,36 +560,6 @@ function departmentReport(name, dept) {
   document.getElementById('dept_name').innerHTML = name;
   document.getElementById('dept_data').value = dept;
   document.getElementById('dept_data_name').value = name;
-}
-
-function switchKiosk() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      setTimeout(function() {location.reload() ;}, 1000);
-    }
-  };
-  xhttp.open('POST', 'index.php?Function=volunteers/kiosk', true);
-  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  xhttp.send('&toggleKiosk=true');
-}
-
-function failKiosk(error) {
-  document.getElementById('kiosk_slider').checked = true;
-  if (error) {
-    window.alert('Login Failed (' + error + ')');
-  }
-}
-
-function toggleKioskMode() {
-  if (kioskMode) {
-    checkAuthentication(userEmail, switchKiosk, failKiosk,
-                        {title: 'Exiting Kiosk Mode'});
-  } else {
-    setTimeout(function() {
-        window.location = 'index.php?Function=volunteers/kiosk';
-      }, 1000);
-  }
 }
 
 function generateCSV() {
