b7e7b5d3d978156b3da48cfcaa6c2ff822b57524
Thomas Keeley <tskeeley@users.noreply.github.com>
2019-02-04 00:04:56 -0600

    cleanup of old aliases - Non affecting - seed data only

--- data/DBSeed/EMails.sql
+++ data/DBSeed/EMails.sql
@@ -1,11 +1,4 @@
 INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (100, 'asl@convergence-con.org', TRUE);
-INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (121, 'gaming-board@convergence-con.org', TRUE);
-INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (121, 'gaming-card@convergence-con.org', TRUE);
-INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (121, 'gaming-computer@convergence-con.org', TRUE);
-INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (121, 'gaming-consuite@convergence-con.org', TRUE);
-INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (121, 'gaming-rpg@convergence-con.org', TRUE);
-INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (127, 'shiftboard@convergence-con.org', TRUE);
-INSERT INTO EMails (DepartmentID, EMail, IsAlias) VALUES (141, 'advertising@convergence-con.org', TRUE);
 INSERT INTO EMails (DepartmentID, Email) VALUES (1, 'activities@convergenceevents.org');
 INSERT INTO EMails (DepartmentID, Email) VALUES (105, 'bookswap@convergence-con.org');
 INSERT INTO EMails (DepartmentID, Email) VALUES (108, 'cinemarex@convergence-con.org');
