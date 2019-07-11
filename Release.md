5df6c0b43fa60c09289a9fb9a6b758f4ff76bf19
Aric Stewart <aric@codeweavers.com>
2019-07-11 13:22:14 -0500

    preliminary release script work

---
+++ Release.md
@@ -0,0 +1,20 @@
+# Release Process for CIAB
+This is an in-progress process and documentation. Expect things to change and update here as the process is more clearly defined and implemented. 
+
+This is the release process for Con-in-a-Box used by CONvergence. It can be the baseline for other conventions or user if so desired, or simply used as a roadmap example of how Con-in-a-Box is released on one site.
+
+There 2 to main development branches that are important. '**Master**' and '**Production**'. 
+
+* **Master** is the development tip and should be considered latest and greatest, as well as unstable. Development change sets are committed here.
+* **Production** is the branch used to build production releases. It is branched from **Master** at stable points.
+
+When a release is being prepared the first step is to update the **Production** branch to the latest **Master**. Then a proper release is tagged from the **Production** branch.
+
+There are tools that are in process and will live in the tools directory that can be installed onto the production web server that will be triggered by web hooks to automatically update a given web server to a release when it is done. 
+
+## Tools
+
+There is a basic release script located in `tools/prepare_release.php` This tool will update the version.ini and tag the branch with an automated tag name. It is again appropriate for CONvergence but may need modification or generalization for other events. 
+
+This tool is meant to be run on the **Production** branch to the the proper tagging. It can take a `--dry-run` option to see what is happening without actually taking any action. 
+
---
+++ tools/prepare_release.php
@@ -0,0 +1,128 @@
+<?php
+
+/*.
+    require_module 'standard';
+.*/
+
+require_once __DIR__."/../functions/functions.inc";
+
+$dry_run = true;
+
+
+function getGitVersion()
+{
+    $dirty = false;
+    $build = intval(exec('git rev-list HEAD|wc -l'));
+    if ($build > 0) {
+        $output = exec('git rev-parse --short HEAD', $data, $result);
+        if ($result != 0) {
+            $rev = "unknown";
+        } else {
+            $rev = $output;
+        }
+        exec('git rev-list -n 1 HEAD --not --remotes=origin', $data, $result);
+        $dirty = ($data == 0);
+        if (!$dirty) {
+            exec(
+                'git update-index --refresh --unmerged -q >/dev/null',
+                $data,
+                $result
+            );
+            $dirty = ($result != 0);
+        }
+        if (!$dirty) {
+            exec(
+                'git diff-index --ignore-submodules=untracked --quiet HEAD',
+                $data,
+                $result
+            );
+            $dirty = ($result != 0);
+        }
+    }
+    return [$build, $dirty];
+
+}
+
+
+function updateVersion($gitver)
+{
+    global $BASEDIR, $dry_run;
+    $version = parse_ini_file($BASEDIR."/version.ini");
+
+    if ($gitver[0] != $version['build']) {
+        $gitver[0] = $gitver[0] + 1;
+        print "-- Updating build number from ".$version['build']." to ".
+              $gitver[0]."\n";
+        $version['build'] = $gitver[0];
+        $output = <<<E
+[Version]
+major={$version['major']}
+minor={$version['minor']}
+build={$version['build']}
+tag={$version['tag']}
+E;
+        if (!$dry_run) {
+            @file_put_contents($BASEDIR."/version.ini", $output);
+            exec("git add $BASEDIR/version.ini");
+            exec('git commit -m "Update version.ini for '.$gitver[0].' build"');
+        }
+    }
+    return $version;
+
+}
+
+
+function tagSource($version)
+{
+    global $dry_run;
+
+    $tag = exec("git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null");
+
+    if ($tag == "") {
+        $newtag = "Release-v".$version['major'].".".$version['minor'].".".$version['build']."-CVG-".date("Y");
+        print "-- Tagging release ".$newtag;
+        if (!$dry_run) {
+            exec("git tag -a \"$newtag\" -m \"Tagging Release $newtag\"");
+        }
+        print "\n";
+        print "--  If you are happy with this tag, push it with:\n";
+        print "       git push origin \"$newtag\"\n";
+        print "--  Otherwise delete it *before it has been pushed*, with:\n";
+        print "       git tag -d \"$newtag\"\n";
+    }
+    return $newtag;
+
+}
+
+function release($tag)
+{
+    $file = "/tmp/$tag.tar.gz";
+    $root = exec("git rev-parse --show-toplevel");
+    $cwd = @getcwd();
+    @chdir($root);
+    exec("git archive --format=tar.gz --prefix ciab/ --output $file HEAD");
+    @chdir($cwd);
+    print "Archive stored '$file'\n";
+}
+
+
+$dry_run = (in_array('--dry-run', $argv));
+
+if ($dry_run) {
+    print" ===============  DRY RUN ================= \n";
+} else {
+    print" ===============  Prepearing Release ================= \n";
+}
+
+$gitver = getGitVersion();
+if (intval($gitver[1]) > 0) {
+    print "** Failing to prepare release: Local tree is unclean\n";
+    if (!$dry_run) {
+        exit(1);
+    }
+}
+$version = updateVersion($gitver);
+$tag = tagSource($version);
+print" ===============  Archiving ================= \n";
+release($tag);
+print" ===============  DONE ================= \n";
