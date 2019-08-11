496b5e8ae7000c8584e3c55cdf6de4cec97631d1
Aric Stewart <aric@codeweavers.com>
2019-08-11 18:08:34 -0500

    oauth2 support work

---
+++ authorize.php
@@ -0,0 +1,32 @@
+<?php
+
+// include our OAuth2 Server object
+require_once __DIR__.'/backends/oauth2.inc';
+
+$request = OAuth2\Request::createFromGlobals();
+$response = new OAuth2\Response();
+
+// validate the authorize request
+if (!$server->validateAuthorizeRequest($request, $response)) {
+    $response->send();
+    die;
+}
+// display an authorization form
+if (empty($_POST)) {
+  exit('
+<form method="post">
+  <label>Do You Authorize TestClient?</label><br />
+  <input type="submit" name="authorized" value="yes">
+  <input type="submit" name="authorized" value="no">
+</form>');
+}
+
+// print the authorization code if the user has authorized your client
+$is_authorized = ($_POST['authorized'] === 'yes');
+$server->handleAuthorizeRequest($request, $response, $is_authorized);
+if ($is_authorized) {
+  // this is only here so that you get to see your code in the cURL request. Otherwise, we'd redirect back to the client
+  $code = substr($response->getHttpHeader('Location'), strpos($response->getHttpHeader('Location'), 'code=')+5, 40);
+  exit("SUCCESS! Authorization Code: $code");
+}
+$response->send();
