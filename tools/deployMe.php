8f02349527bfb4d2ab6c04c0871f5fda24a164f6
Thomas Keeley <tskeeley@users.noreply.github.com>
2019-10-16 14:45:46 -0500

    Support/Fix GitHub auto deployments
    Add a .env read to grab GitHub Auto Deployment.  Should likely move this to the Configuration Table since it requires a stable initial deployment first.

--- tools/deployMe.php
+++ tools/deployMe.php
@@ -11,6 +11,13 @@
     require_module 'hash';
 .*/
 
+// Load in the .env file, if it exists
+require __DIR__."/../vendor/autoload.php";
+if (is_file(__DIR__.'/../.env')) {
+   $dotenv = Dotenv\Dotenv::create(__DIR__.'/../');
+   $dotenv->load();
+}
+
 $secret = getenv('GITHUB_SECRET');
 if ($secret === false) {
     die('GITHUB_SECRET not defined');
