3fb0dda30807b65452043d53c98414ac1139e15d
Aric Stewart <aric@codeweavers.com>
2019-10-28 07:51:05 -0500

    Do not set example Neon data by default

--- data/DBSeed/Configuration.sql
+++ data/DBSeed/Configuration.sql
@@ -6,8 +6,6 @@ INSERT INTO Configuration (Field, Value) VALUES ('HELP_EMAIL', '');
 INSERT INTO Configuration (Field, Value) VALUES ('NOREPLY_EMAIL', '');
 INSERT INTO Configuration (Field, Value) VALUES ('ADMINACCOUNTS', '3218,42966');
 INSERT INTO Configuration (Field, Value) VALUES ('DUMPSECURE', 'MPfKCJuEn7ZN2ypDc564NNXHAeGq6mc8dcNVYfXhg4FKq85d6K');
-INSERT INTO Configuration (Field, Value) VALUES ('NEONID', 'home');
-INSERT INTO Configuration (Field, Value) VALUES ('NEONKEY', '3a9236b6c4f3cae82aa46509971ac4b2');
 INSERT INTO Configuration (Field, Value) VALUES ('TIMEZONE', 'America/Chicago');
 INSERT INTO Configuration (Field, Value) VALUES ('DBSchemaVersion', '0');
 INSERT INTO Configuration (Field, Value) VALUES ('CONCOMHOURS', '60');
