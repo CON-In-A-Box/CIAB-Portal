77a6d324d91d7687392c776c763b115d7ef201cd
Aric Stewart <aric@codeweavers.com>
2019-11-05 19:06:30 -0600

    Add phpmyadmin to docker swarm

--- docker_instance.sh
+++ docker_instance.sh
@@ -2,9 +2,8 @@
 
 if [ "$1" == "up" ]; then
     shift
-    DBUSER="ciabuser" DBNAME="ciab" DBPASS="ciabpass" docker-compose up $@;
+    DBUSER="ciabuser" DBNAME="ciab" DBPASS="ciabpass" docker-compose -f docker-compose.yml -f docker-compose.phpmyadmin.yml up
 elif [ "$1" == "down" ]; then
     shift
-    docker-compose down $@;
+    docker-compose -f docker-compose.yml -f docker-compose.phpmyadmin.yml down $@;
 fi
-
