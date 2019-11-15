8d919fe518163d43cc20a73f76cd1051f384ef90
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2019-11-14 21:43:55 -0600

    Tweaks for dockerverse

--- docker_instance.sh
+++ docker_instance.sh
@@ -2,7 +2,8 @@
 
 if [ "$1" == "up" ]; then
     shift
-    DBUSER="ciabuser" DBNAME="ciab" DBPASS="ciabpass" docker-compose -f docker-compose.yml -f docker-compose.phpmyadmin.yml up
+    chown o+w resources
+    DBUSER="ciabuser" DBNAME="ciab" DBPASS="ciabpass" DB_BACKEND="mysqlpdo.inc" docker-compose -f docker-compose.yml -f docker-compose.phpmyadmin.yml up
 elif [ "$1" == "down" ]; then
     shift
     docker-compose -f docker-compose.yml -f docker-compose.phpmyadmin.yml down $@;
