aaaa9d63e6f03f0a1400ab4175c84a0bff2abc5c
Aric Stewart <aric@codeweavers.com>
2020-06-02 09:00:00 -0500

    First pass on OAuth2 Authentication dialog

--- api/src/App/OAuth2.php
+++ api/src/App/OAuth2.php
@@ -5,13 +5,30 @@
 
 use Chadicus\Slim\OAuth2\Routes;
 use Slim\Views;
+use Psr\Http\Message\ServerRequestInterface;
 
 require_once __DIR__.'/../../../backends/oauth2.inc';
 
 /* oauth2 stuff */
 
-$renderer = new Views\PhpRenderer(__DIR__.'/../../../vendor/chadicus/slim-oauth2-routes/templates');
+$renderer = new Views\PhpRenderer(__DIR__.'/../Templates');
 
-$app->map(['GET', 'POST'], Routes\Authorize::ROUTE, new Routes\Authorize($server, $renderer))->setName('authorize');
+class MyUserIdProvider implements Routes\UserIdProviderInterface
+{
+
+
+    public function getUserId(ServerRequestInterface $request, array $arguments = [])
+    {
+        $body = $request->getParsedBody();
+        return isset($body['user_id']) ? $body['user_id'] : null;
+
+    }
+
+
+    /* End MyUserIdProvider */
+}
+
+
+$app->map(['GET', 'POST'], Routes\Authorize::ROUTE, new Routes\Authorize($server, $renderer, 'authorize.phtml', new MyUserIdProvider))->setName('authorize');
 $app->post(Routes\Token::ROUTE, new Routes\Token($server))->setName('token');
 $app->map(['GET', 'POST'], Routes\ReceiveCode::ROUTE, new Routes\ReceiveCode($renderer))->setName('receive-code');
