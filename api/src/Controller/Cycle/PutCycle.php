8767e9279ac5248b5c080e4897c101ff955dfcfa
Aric Stewart <aric@codeweavers.com>
2020-10-11 20:58:37 -0500

    Fix reversed POST and PUT

--- api/src/Controller/Cycle/PutCycle.php
+++ api/src/Controller/Cycle/PutCycle.php
@@ -12,36 +12,47 @@ class PutCycle extends BaseCycle
 {
 
 
-    public function buildResource(Request $request, Response $response, $args): array
+    public function buildResource(Request $request, Response $response, $params): array
     {
+        $sth = $this->container->db->prepare("SELECT * FROM `AnnualCycles` WHERE `AnnualCycleID` = ".$params['id']);
+        $sth->execute();
+        $cycles = $sth->fetchAll();
+        if (empty($cycles)) {
+            return [
+            \App\Controller\BaseController::RESULT_TYPE,
+            $this->errorResponse($request, $response, 'Not Found', 'Cycle Not Found', 404)];
+        }
         if (!\ciab\RBAC::havePermission('api.put.cycle')) {
             return [
             \App\Controller\BaseController::RESULT_TYPE,
             $this->errorResponse($request, $response, 'Permission Denied', 'Permission Denied', 403)];
         }
-        $body = $request->getParsedBody();
-        if (!array_key_exists('From', $body)) {
-            return [
-            \App\Controller\BaseController::RESULT_TYPE,
-            $this->errorResponse($request, $response, 'Required \'From\' parameter not present', 'Missing Parameter', 400)];
-        }
-        if (!array_key_exists('To', $body)) {
-            return [
-            \App\Controller\BaseController::RESULT_TYPE,
-            $this->errorResponse($request, $response, 'Required \'To\' parameter not present', 'Missing Parameter', 400)];
-        }
-        $from = date_format(new \DateTime($body['From']), 'Y-m-d');
-        $to = date_format(new \DateTime($body['To']), 'Y-m-d');
-        $sql = "INSERT INTO `AnnualCycles` (`AnnualCycleID`, `DateFrom`, `DateTo`) VALUES (NULL, '$from', '$to')";
-        $sth = $this->container->db->prepare($sql);
-        $sth->execute();
 
-        $target = new \App\Controller\Cycle\GetCycle($this->container);
-        $data = $target->buildResource($request, $response, ['id' => $this->container->db->lastInsertId()])[1];
-        return [
-        \App\Controller\BaseController::RESOURCE_TYPE,
-        $target->arrayResponse($request, $response, $data)
-        ];
+
+        $sql = "UPDATE `AnnualCycles` SET ";
+
+        $body = $request->getParsedBody();
+        $changes = array();
+
+        if (array_key_exists('From', $body)) {
+            $from = date_format(new \DateTime($body['From']), 'Y-m-d');
+            $changes[] = "`DateFrom` = '$from'";
+        }
+
+        if (array_key_exists('To', $body)) {
+            $to = date_format(new \DateTime($body['To']), 'Y-m-d');
+            $changes[] = "`DateTo` = '$to'";
+        }
+
+        if (count($changes) > 0) {
+            $sql .= implode(',', $changes);
+            $sql .= " WHERE `AnnualCycleID` = '".$params['id']."';";
+
+            print $sql;
+            $sth = $this->container->db->prepare($sql);
+            $sth->execute();
+        }
+        return [null];
 
     }
 
