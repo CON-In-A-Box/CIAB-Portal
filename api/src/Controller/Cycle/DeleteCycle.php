d45792a214bbfa21ec55e01d9de10897c4cd2afd
Aric Stewart <aric@codeweavers.com>
2020-10-14 19:10:29 -0500

    Reduce code duplication in Cycle api

--- api/src/Controller/Cycle/DeleteCycle.php
+++ api/src/Controller/Cycle/DeleteCycle.php
@@ -14,13 +14,9 @@ class DeleteCycle extends BaseCycle
 
     public function buildResource(Request $request, Response $response, $params): array
     {
-        $sth = $this->container->db->prepare("SELECT * FROM `AnnualCycles` WHERE `AnnualCycleID` = ".$params['id']);
-        $sth->execute();
-        $cycles = $sth->fetchAll();
-        if (empty($cycles)) {
-            return [
-            \App\Controller\BaseController::RESULT_TYPE,
-            $this->errorResponse($request, $response, 'Not Found', 'Cycle Not Found', 404)];
+        $error = $this->getCycle($cycles, $params);
+        if ($error) {
+            return $error;
         }
         if (!\ciab\RBAC::havePermission('api.delete.cycle')) {
             return [
