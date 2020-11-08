be8bd6792ef83912aa25e0c7c78b9a136211a6cd
Aric Stewart <aric@codeweavers.com>
2020-11-07 20:59:06 -0600

    Add database tables for configuration options and per account configuration

---
+++ data/DBSeed/ConfigurationTypes.sql
@@ -0,0 +1,4 @@
+INSERT INTO ConfigurationTypes (Type) VALUES ('boolean');
+INSERT INTO ConfigurationTypes (Type) VALUES ('text');
+INSERT INTO ConfigurationTypes (Type) VALUES ('integer');
+INSERT INTO ConfigurationTypes (Type) VALUES ('select');
