1a1e7deed03bcbbbf96c959a0b51069233a63d70
Aric Stewart <aric@codeweavers.com>
2020-11-11 09:53:37 -0600

    Send email to relevent members on new announcement

--- api/src/Controller/Announcement/PutAnnouncement.php
+++ api/src/Controller/Announcement/PutAnnouncement.php
@@ -5,13 +5,109 @@
 
 namespace App\Controller\Announcement;
 
+require_once __DIR__.'/../../../../backends/email.inc';
+
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Views;
 
 require_once __DIR__.'/../../../../functions/users.inc';
 
 class PutAnnouncement extends BaseAnnouncement
 {
+
+
+    private function sendEmail($department, $scope, $text)
+    {
+        global $CONSITENAME, $BASEURL;
+
+        $condition = '';
+
+        if ($scope == 1) {
+            $condition = <<<SQL
+                AND (
+                    SELECT
+                        COUNT(AccountID)
+                    FROM
+                        `ConComList`
+                    WHERE
+                        `AccountID`  = d.AccountID
+                ) > 0
+SQL;
+        } elseif ($scope == 2) {
+            $condition = <<<SQL
+              AND (
+                {$department['id']} IN(
+                SELECT
+                    `DepartmentID`
+                FROM
+                    `ConComList`
+                WHERE
+                    `AccountID` = d.AccountID
+            ) OR {$department['id']} IN(
+                SELECT
+                    `DepartmentID`
+                FROM
+                    `Departments`
+                WHERE
+                    `ParentDepartmentID` IN(
+                    SELECT
+                        `DepartmentID`
+                    FROM
+                        `ConComList`
+                    WHERE
+                        `AccountID` = d.AccountID
+                )
+            ))
+SQL;
+        }
+        $sql = <<<SQL
+            SELECT
+               Email, firstName
+            FROM
+                (
+                SELECT
+                    m.*,
+                    CASE WHEN ac.`Value` IS NOT NULL THEN ac.`Value`
+                         ELSE af.`InitialValue`
+            END AS `Value`
+            FROM
+                `Members` AS m
+            LEFT JOIN `AccountConfiguration` AS ac
+            ON
+                m.AccountID = ac.AccountID AND ac.FIELD = 'AnnounceEmail'
+                AND ac.Value IS NOT NULL
+            LEFT JOIN `ConfigurationField` AS af
+            ON
+                af.Field = 'AnnounceEmail'
+            ) AS d
+            WHERE
+                `Value` = 1
+                $condition
+SQL;
+        $sth = $this->container->db->prepare($sql);
+        $sth->execute();
+        $members = $sth->fetchAll();
+
+        $subject = "$CONSITENAME New Announcement";
+
+        foreach ($members as $target) {
+            $phpView = new Views\PhpRenderer(__DIR__.'/../../Templates', [
+                'site' => $BASEURL,
+                'department' => $department['Name'],
+                'announcement' => $text,
+                'con' => $CONSITENAME,
+                'url' => $BASEURL.'?Function=configuration',
+            ]);
+            $response = new Response();
+            $phpView->render($response, 'newAnnouncements.phtml');
+            $response->getBody()->rewind();
+            $message = $response->getBody()->getContents();
+
+            \ciab\Email::mail($target['Email'], \getNoReplyAddress(), $subject, $message);
+        }
+
+    }
 
 
     public function buildResource(Request $request, Response $response, $args): array
@@ -48,6 +144,10 @@ class PutAnnouncement extends BaseAnnouncement
 
             $sth = $this->container->db->prepare("INSERT INTO `Announcements` (DepartmentID, PostedBy, PostedOn, Scope, Text) VALUES ({$department['id']}, $member, now(), '{$body['Scope']}', $text)");
             $sth->execute();
+
+            if (!array_key_exists('Email', $body) || boolval($body['Email'])) {
+                $this->sendEmail($department, intval($body['Scope']), $text);
+            }
             return [null];
         } else {
             return [
