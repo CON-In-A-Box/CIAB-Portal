69c12a3e47493ca2ab5eb182852fae8a445ff762
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2020-12-19 17:32:42 -0600

    Delete obsolete

--- checkout.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head>
-    <title>Register for CONvergence</title>
-    <script src="https://js.stripe.com/v3/"></script>
-  </head>
-  <body>
-    <button id="checkout-button">Checkout</button>
-  </body>
-</html>
