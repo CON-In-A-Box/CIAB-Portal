43c64b30e3ceb07f39aba28324f87d1c365aab07
Aric Stewart <aric@codeweavers.com>
2021-01-12 07:44:35 -0600

    Add API configuration class

---
+++ sitesupport/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "eslint:recommended",
+  "overrides": [{
+    "files": ["apiConfiguration.js"],
+    "parserOptions": {
+      "sourceType": "module"
+    }
+  }]
+}
