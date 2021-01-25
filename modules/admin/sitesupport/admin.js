d1183824a9d31bb867eeefa753733c56fe8ba9bf
Aric Stewart <aric@codeweavers.com>
2021-01-25 11:47:29 -0600

    Switch to API for activity log and load on demand

--- modules/admin/sitesupport/admin.js
+++ modules/admin/sitesupport/admin.js
@@ -3,7 +3,7 @@
 /* globals apiRequest, confirmbox, basicBackendRequest, showSpinner,
            hideSpinner, alertbox */
 /* exported downloadLog, addField, removeAdmin, updateMemberships,
-            doSUDO, rebuildSCSS, setPassword */
+            doSUDO, rebuildSCSS, setPassword, getLog */
 
 function downloadLog() {
   window.location = 'index.php?Function=admin&downloadLog=db';
@@ -61,5 +61,59 @@ function setPassword() {
     .catch(function(response) {
       hideSpinner();
       alertbox('Password Reset Failed: ' + response.responseText);
+    });
+}
+
+function addLogRow(data) {
+  var e = document.createElement('DIV');
+  e.classList.add('ADMIN-log-row');
+  var d = document.createElement('DIV');
+  d.classList.add('ADMIN-log-col-date');
+  d.innerHTML = data.Date;
+  e.append(d);
+  d = document.createElement('DIV');
+  d.classList.add('ADMIN-log-col-id');
+  d.innerHTML = data.AccountID;
+  e.append(d);
+  d = document.createElement('DIV');
+  d.classList.add('ADMIN-log-col-function');
+  d.innerHTML = data.Function;
+  e.append(d);
+  d = document.createElement('DIV');
+  d.classList.add('ADMIN-log-col-query');
+  var t = data.Query.trim();
+  if (t.length > 100) {
+    var p = document.createElement('P');
+    p.classList.add('UI-tooltip');
+    p.innerHTML = t.slice(0,100) + ' \u{2026}';
+    var s = document.createElement('SPAN');
+    s.classList.add('UI-tooltip-text');
+    s.innerHTML = t;
+    p.append(s);
+    d.append(p);
+  } else {
+    d.innerHTML = t;
+  }
+  e.append(d);
+
+  document.getElementById('LogEntries').append(e);
+
+}
+
+function getLog(size) {
+  document.getElementById('LogEntries').innerHTML = '';
+  if (!document.getElementById('db_log').classList.contains('UI-show')) {
+    return;
+  }
+  showSpinner();
+  apiRequest('GET', 'admin/log/' + size, null)
+    .then(function(response) {
+      var d = JSON.parse(response.responseText);
+      d.data.forEach(addLogRow);
+      hideSpinner();
+    })
+    .catch(function(response) {
+      if (response instanceof Error) { throw response; }
+      hideSpinner();
     });
 }
