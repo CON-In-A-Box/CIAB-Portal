e96696fc138f8c192042ada5df5b9ffc9aa0d673
Aric Stewart <aric@codeweavers.com>
2021-02-06 22:07:58 -0600

    Reduce duplication in ticket API

--- api/src/Modules/registration/Controller/Ticket/UnvoidTicket.php
+++ api/src/Modules/registration/Controller/Ticket/UnvoidTicket.php
@@ -14,30 +14,17 @@ class UnvoidTicket extends BaseTicket
 
     public function buildResource(Request $request, Response $response, $params): array
     {
-        if (!\ciab\RBAC::havePermission('api.registration.ticket.unvoid')) {
-            return [
-            \App\Controller\BaseController::RESULT_TYPE,
-            $this->errorResponse($request, $response, 'Permission Denied', 'Permission Denied', 403)];
-        }
         $id = $params['id'];
         $sql = "UPDATE `Registrations` SET `VoidDate` = NULL, `VoidBy` = NULL, `VoidReason` = NULL WHERE RegistrationID = $id AND `VoidDate` IS NOT NULL";
 
-        $sth = $this->container->db->prepare($sql);
-        $sth->execute();
-        if ($sth->rowCount() == 0) {
-            return [
-            \App\Controller\BaseController::RESULT_TYPE,
-            $this->errorResponse($request, $response, 'Conflict', 'Could not update', 409)];
-        }
-
-        $target = new GetTicket($this->container);
-        $newdata = $target->buildResource($request, $response, $params)[1];
-        $data = $target->arrayResponse($request, $response, $newdata);
-
-        return [
-        \App\Controller\BaseController::RESOURCE_TYPE,
-        $data
-        ];
+        return $this->updateTicket(
+            $request,
+            $response,
+            $params,
+            'api.registration.ticket.unvoid',
+            $sql,
+            'Could not update.'
+        );
 
     }
 
