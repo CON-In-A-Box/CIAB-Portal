105c14d5e9e4349bf035e0c6c1e3cbf47eff435b
Aric Stewart <aric@codeweavers.com>
2021-02-21 09:38:38 -0600

    Clean up and modualize the API server a bit

--- api/src/App/App.php
+++ api/src/App/App.php
@@ -3,20 +3,30 @@
     require_module 'standard';
 .*/
 
+use Chadicus\Slim\OAuth2\Middleware;
+
 require __DIR__.'/../../../vendor/autoload.php';
 require __DIR__.'/../../../functions/functions.inc';
+require __DIR__.'/Dependencies.php';
+require __DIR__.'/Routes.php';
+require __DIR__.'/OAuth2.php';
 
 if (is_file(__DIR__.'/../../../.env')) {
     $dotenv = Dotenv\Dotenv::create(__DIR__.'/../../..');
     $dotenv->load();
 }
 
+/* Defines $server as the OAuth2 server */
+require_once __DIR__.'/../../../backends/oauth2.inc';
+
 $settings = require __DIR__.'/Settings.php';
 $app = new \Slim\App($settings);
-require __DIR__.'/Dependencies.php';
-require __DIR__.'/OAuth2.php';
-require __DIR__.'/Middleware.php';
-require __DIR__.'/Routes.php';
+$container = $app->getContainer();
+setupAPIDependencies($app, $settings);
+setupAPIOAuth2($app, $server);
+$authMiddleware = new Middleware\Authorization($server, $container);
+setupAPIRoutes($app, $authMiddleware);
+
 $modules = scandir(__DIR__.'/../Modules');
 foreach ($modules as $key => $value) {
     if (!in_array($value, array(',', '..'))) {
@@ -24,11 +34,28 @@ foreach ($modules as $key => $value) {
             continue;
         }
         if (is_dir(__DIR__.'/../Modules/'.$value)) {
-            if (is_file(__DIR__.'/../Modules/'.$value.'/App/Dependencies.php')) {
-                include(__DIR__.'/../Modules/'.$value.'/App/Dependencies.php');
-            }
-            if (is_file(__DIR__.'/../Modules/'.$value.'/App/Routes.php')) {
-                include(__DIR__.'/../Modules/'.$value.'/App/Routes.php');
+            if (is_file(__DIR__.'/../Modules/'.$value.'/Settings.php')) {
+                $module_settings = include(__DIR__.'/../Modules/'.$value.'/Settings.php');
+                if (is_file(__DIR__.'/../Modules/'.$value.'/App/Dependencies.php')) {
+                    include(__DIR__.'/../Modules/'.$value.'/App/Dependencies.php');
+                }
+                if (is_file(__DIR__.'/../Modules/'.$value.'/App/Routes.php')) {
+                    include(__DIR__.'/../Modules/'.$value.'/App/Routes.php');
+                }
+
+                if (array_key_exists('setupRoutes', $module_settings)) {
+                    call_user_func($module_settings['setupRoutes'], $app, $authMiddleware);
+                }
+                if (array_key_exists('setupDependencies', $module_settings)) {
+                    call_user_func($module_settings['setupDependencies'], $app, $module_settings);
+                }
+
+                $app_settings = $container->get('settings');
+                $app_modules = $app_settings['modules'];
+                $app_modules[] = $module_settings['module'];
+                $app_settings->replace([
+                    'modules' => $app_modules
+                ]);
             }
         }
     }
--- api/src/Modules/concom/App/Routes.php
+++ api/src/Modules/concom/App/Routes.php
@@ -3,27 +3,23 @@
     require_module 'standard';
 .*/
 
-$app->group(
-    '/member',
-    function () use ($app, $authMiddleware) {
-        $app->get('/concom/', 'App\Modules\concom\Controller\GetMemberPosition');
-        $app->get('/{id}/concom', 'App\Modules\concom\Controller\GetMemberPosition');
-    }
-)->add(new App\Middleware\CiabMiddleware($app))->add($authMiddleware);
 
-$app->group(
-    '/department',
-    function () use ($app, $authMiddleware) {
-        $app->get('/concom/', 'App\Modules\concom\Controller\ListConcom');
-        $app->get('/{id}/concom', 'App\Modules\concom\Controller\GetDepartment');
-    }
-)->add(new App\Middleware\CiabMiddleware($app))->add($authMiddleware);
+function setupConComAPI($app, $authMiddleware)
+{
+    $app->group(
+        '/member',
+        function () use ($app, $authMiddleware) {
+            $app->get('/concom/', 'App\Modules\concom\Controller\GetMemberPosition');
+            $app->get('/{id}/concom', 'App\Modules\concom\Controller\GetMemberPosition');
+        }
+    )->add(new App\Middleware\CiabMiddleware($app))->add($authMiddleware);
 
+    $app->group(
+        '/department',
+        function () use ($app, $authMiddleware) {
+            $app->get('/concom/', 'App\Modules\concom\Controller\ListConcom');
+            $app->get('/{id}/concom', 'App\Modules\concom\Controller\GetDepartment');
+        }
+    )->add(new App\Middleware\CiabMiddleware($app))->add($authMiddleware);
 
-$container = $app->getContainer();
-$settings = $container->get('settings');
-$modules = $settings['modules'];
-$modules[] = 'App\Modules\concom\ModuleConcom';
-$settings->replace([
-    'modules' => $modules
-]);
+}
