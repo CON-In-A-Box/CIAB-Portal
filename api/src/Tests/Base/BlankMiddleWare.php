02cedaff99eb0f28e4749e2d94bf6b764a066969
Aric Stewart <aric@codeweavers.com>
2021-03-01 14:55:11 -0600

    Use OAuth2 in testing

--- api/src/Tests/Base/BlankMiddleWare.php
+++ api/src/Tests/Base/BlankMiddleWare.php
@@ -12,6 +12,7 @@ class BlankMiddleWare
 
     public function __invoke(Request $request, Response $response, $next)
     {
+        $request = $request->withAttribute('oauth2-token', ['user_id' => 1000]);
         return $next($request, $response);
 
     }
