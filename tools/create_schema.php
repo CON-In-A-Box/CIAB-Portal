26effb6049a36d472b3525f9c21b819d5aebd835
Aric Stewart <aric@codeweavers.com>
2021-03-02 08:01:21 -0600

    Ensure database is updated before running tests

--- tools/create_schema.php
+++ tools/create_schema.php
@@ -1,5 +1,8 @@
 <?php
 
 require_once(__DIR__.'/../functions/database.inc');
+require_once(__DIR__.'/../data/database-schema.php');
+require_once(__DIR__.'/../functions/update.inc');
 
 $db = new DB();
+\db_do_update(0, SCHEMA::$REQUIED_DB_SCHEMA);
