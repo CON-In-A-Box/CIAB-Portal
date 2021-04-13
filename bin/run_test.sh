df714a567f6c6663beae2a9a7514765ca5ce64e7
Aric Stewart <aric@codeweavers.com>
2021-04-12 21:02:02 -0500

    Ensure DB up before testing

--- bin/run_test.sh
+++ bin/run_test.sh
@@ -2,6 +2,7 @@
 source .env
 export DBNAME=${TEST_DBNAME}
 
+docker-compose run --rm php php tools/check_db.php
 docker-compose run --rm php php tools/create_schema.php
 docker-compose run --rm php php tools/test_prereqs.php
 docker-compose run --rm php ./vendor/bin/phpunit --configuration phpunit.xml api $@
