1c07f855506025c18e58d18a3016f070714db115
Aric Stewart <aric@codeweavers.com>
2023-08-07 10:39:59 -0500

    Volunteer improvements
    Reduce full page reloads
    Clearly display selected volunteers name
    Shift a few more things to vue and out of raw javascript.

--- modules/volunteers/sitesupport/hourTable.js
+++ modules/volunteers/sitesupport/hourTable.js
@@ -20,81 +20,7 @@ export default {
     'hourChange'
   ],
   mounted() {
-    showSpinner();
-    this.summeryColumns = [
-      {value:'name', title:'Department Worked', source:null},
-      {value:'entry_count', title:'Total Entries', source:this.hours},
-      {value:'volunteer_count', title:'Total Volunteers', source:this.hours},
-      {value:'total_hours', title:'Total Hours', source: this.hours}
-    ];
-    this.detailColumns = [
-      {value:'name', title:'Department Worked', source:null},
-      {value:'end', title:'Ended', source: this.hours},
-      {value:'hours', title:'Hours', source: this.hours}
-    ];
-
-    apiRequest('GET', '/department','max_results=all')
-      .then((response) => {
-        const result = JSON.parse(response.responseText);
-        this.departments = [];
-        result.data.forEach((entry) => {
-          this.departments.push(entry);
-        });
-
-        this.departments.sort((a,b) => (a.name > b.name) ? 1 : -1);
-
-        if (this.$parent.userId != null) {
-          this.columns = this.detailColumns;
-          this.records = this.hours;
-          apiRequest('GET', '/member/' + this.$parent.userId + '/volunteer/hours','max_results=all')
-            .then((response) => {
-              const result = JSON.parse(response.responseText);
-              result.data.forEach((entry) => {
-                this.hours.push(entry);
-              })
-              this.hours.sort((a,b) => (a.department.name > b.department.name) ? 1 : -1);
-              this.totalSpentHours = 0;
-              this.totalHours = result.total_hours;
-              if (this.footer && this.$parent.userId != null) {
-                apiRequest('GET', '/member/' + this.$parent.userId + '/volunteer/claims/summary','max_results=all')
-                  .then((response) => {
-                    const result = JSON.parse(response.responseText);
-                    this.totalSpentHours = result.spent_hours;
-                  })
-                  .finally(() => {
-                    hideSpinner();
-                    this.$emit('hourChange', this.totalHours, this.totalSpentHours);
-                  })
-              } else {
-                hideSpinner();
-                this.$emit('hourChange', this.totalHours, this.totalSpentHours);
-              }
-            })
-        } else {
-          this.columns = this.summeryColumns;
-          this.records = this.departments;
-          apiRequest('GET', '/event/current/volunteer/hours/summary','max_results=all')
-            .then((response) => {
-              const result = JSON.parse(response.responseText);
-              result.data.forEach((entry) => {
-                this.hours[entry.department.id] = entry;
-              })
-              this.totalVolunteers = result.total_volunteer_count;
-              this.totalHours = result.total_hours;
-              apiRequest('GET', '/event/current/volunteer/claims/summary','max_results=all')
-                .then((response) => {
-                  const result = JSON.parse(response.responseText);
-                  this.totalSpentHours = result.spent_hours;
-                })
-                .finally(() => {
-                  hideSpinner();
-                })
-            })
-            .finally(() => {
-              hideSpinner();
-            })
-        }
-      })
+    this.load();
   },
   created() {
   },
@@ -110,6 +36,86 @@ export default {
     }
   },
   methods: {
+    load() {
+      this.records = null;
+      this.totalHours = 0;
+      this.totalSpentHours = 0;
+      this.hours = [];
+      this.totalVolunteers = null;
+
+      showSpinner();
+      this.summeryColumns = [
+        {value:'name', title:'Department Worked', source:null},
+
+        {value:'entry_count', title:'Total Entries', source:this.hours},
+        {value:'volunteer_count', title:'Total Volunteers', source:this.hours},
+        {value:'total_hours', title:'Total Hours', source: this.hours}
+      ];
+      this.detailColumns = [
+        {value:'name', title:'Department Worked', source:null},
+        {value:'end', title:'Ended', source: this.hours},
+        {value:'hours', title:'Hours', source: this.hours}
+      ];
+
+      apiRequest('GET', '/department','max_results=all')
+        .then((response) => {
+          const result = JSON.parse(response.responseText);
+          this.departments = result.data;
+          this.departments.sort((a,b) => (a.name > b.name) ? 1 : -1);
+
+          if (this.$parent.userId != null) {
+            this.columns = this.detailColumns;
+            this.records = this.hours;
+            apiRequest('GET', '/member/' + this.$parent.userId + '/volunteer/hours','max_results=all')
+              .then((response) => {
+                const result = JSON.parse(response.responseText);
+                result.data.forEach((entry) => {
+                  this.hours.push(entry);
+                })
+                this.hours.sort((a,b) => (a.department.name > b.department.name) ? 1 : -1);
+                this.totalSpentHours = 0;
+                this.totalHours = result.total_hours;
+                if (this.footer && this.$parent.userId != null) {
+                  apiRequest('GET', '/member/' + this.$parent.userId + '/volunteer/claims/summary','max_results=all')
+                    .then((response) => {
+                      const result = JSON.parse(response.responseText);
+                      this.totalSpentHours = result.spent_hours;
+                    })
+                    .finally(() => {
+                      hideSpinner();
+                      this.$emit('hourChange', this.totalHours, this.totalSpentHours);
+                    })
+                } else {
+                  hideSpinner();
+                  this.$emit('hourChange', this.totalHours, this.totalSpentHours);
+                }
+              })
+          } else {
+            this.columns = this.summeryColumns;
+            this.records = this.departments;
+            apiRequest('GET', '/event/current/volunteer/hours/summary','max_results=all')
+              .then((response) => {
+                const result = JSON.parse(response.responseText);
+                result.data.forEach((entry) => {
+                  this.hours[entry.department.id] = entry;
+                })
+                this.totalVolunteers = result.total_volunteer_count;
+                this.totalHours = result.total_hours;
+                apiRequest('GET', '/event/current/volunteer/claims/summary','max_results=all')
+                  .then((response) => {
+                    const result = JSON.parse(response.responseText);
+                    this.totalSpentHours = result.spent_hours;
+                  })
+                  .finally(() => {
+                    hideSpinner();
+                  })
+              })
+              .finally(() => {
+                hideSpinner();
+              })
+          }
+        })
+    },
     /*eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }]*/
     clicked(_record) {},
     printHours(value) {
@@ -147,12 +153,14 @@ export default {
       }
 
       return 0;
-    },
+    }
   },
   template: `
   <div class="UI-container UI-center event-color-primary" :style="styles">
     <div class="UI-stripedtable">
-      <div class='UI-tabletitle'>{{title}}</div>
+      <div class='UI-tabletitle'>{{title}}
+        <span v-if="$parent.userData"> ({{$parent.userData.first_name}}  {{$parent.userData.last_name}})</span>
+      </div>
       <div class="UI-padding">
         <div class="UI-table-all">
           <div class="UI-table-row">
--- modules/volunteers/sitesupport/volunteers.js
+++ modules/volunteers/sitesupport/volunteers.js
@@ -4,46 +4,11 @@
 
 /* jshint browser: true */
 /* jshint -W097 */
-/* globals userEmail, checkAuthentication, adminMode, alertbox */
-/* exported  generateDeptReport, toggleAdminMode, sidebarMainDiv */
+/* exported  generateDeptReport, sidebarMainDiv */
 
 'use strict';
 
 var sidebarMainDiv = 'info_div';
-
-function enterAdmin() {
-  setTimeout(location.reload(), 1000);
-}
-
-function failAdmin(error) {
-  document.getElementById('admin_slider').checked = false;
-  if (error) {
-    alertbox('Login Failed (' + error + ')');
-  }
-}
-
-function toggleAdminMode() {
-  document.cookie = 'CIAB_VOLUNTEERADMIN=;expires=Thu, 01 Jan 1970 ' +
-                    '00:00:01 GMT;';
-  var target = '';
-  var userId = null;
-  const searchParams = new URLSearchParams(window.location.search);
-  if (searchParams.has('volunteerId')) {
-    userId = searchParams.get('volunteerId');
-  }
-  if (userId) {
-    target = 'index.php?Function=volunteers/admin&volunteerId=' + userId;
-  } else {
-    target = 'index.php?Function=volunteers/admin';
-  }
-  if (!adminMode) {
-    checkAuthentication(userEmail, enterAdmin, failAdmin,
-      {target: 'volunteers/admin'});
-  } else {
-    setTimeout(function() {window.location = target;}, 1000);
-  }
-}
-
 
 function generateDeptReport() {
   var name = document.getElementById('dept_data_name').value;
