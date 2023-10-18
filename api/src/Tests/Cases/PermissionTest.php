78b256b2f67f1c8a6847f50efabde468aeac95b6
Aric Stewart <aric@codeweavers.com>
2023-10-18 10:23:45 -0500

    Correction to Member RBAC checks

--- api/src/Tests/Cases/PermissionTest.php
+++ api/src/Tests/Cases/PermissionTest.php
@@ -20,6 +20,47 @@ class PermissionsTest extends CiabTestCase
         $this->assertEquals($data->type, 'permission_list');
         $this->assertNotEmpty($data->data);
 
+        $id = $this->testing_accounts[0];
+
+        $data = $this->NPRunSuccessJsonRequest('GET', "/member/$id/permissions");
+        $this->assertEquals($data->type, 'permission_list');
+        $this->assertEmpty($data->data);
+
+        $data = $this->NPRunSuccessJsonRequest('GET', '/permissions/generic/announcement/put/all');
+        $this->assertEquals($data->type, 'permission_list');
+        $this->assertEquals($data->data[0]->allowed, 0);
+
+        $data = $this->NPRunSuccessJsonRequest('GET', '/permissions/generic/bad/get/one');
+        $this->assertEquals($data->type, 'permission_list');
+        $this->assertEquals($data->data[0]->allowed, 0);
+
+        $data = $this->RunSuccessJsonRequest('GET', '/permissions/generic/announcement/put/all');
+        $this->assertEquals($data->type, 'permission_list');
+        $this->assertEquals($data->data[0]->allowed, 1);
+
+        $data = $this->RunSuccessJsonRequest('GET', '/permissions/generic/bad/get/one');
+        $this->assertEquals($data->type, 'permission_list');
+        $this->assertEquals($data->data[0]->allowed, 1);
+
+    }
+
+
+    public function testStaffPermissions(): void
+    {
+        $id = $this->testing_accounts[0];
+
+        $data = $this->NPRunSuccessJsonRequest('GET', "/member/$id/permissions");
+        $this->assertEquals($data->type, 'permission_list');
+        $this->assertEmpty($data->data);
+
+        $staff_record = $this->runSuccessJsonRequest('POST', "/member/$id/staff_membership", null, ['Department' => 1, 'Position' => 1], 201);
+
+        $data = $this->NPRunSuccessJsonRequest('GET', "/member/$id/permissions");
+        $this->assertEquals($data->type, 'permission_list');
+        $this->assertNotEmpty($data->data);
+
+        $this->runRequest('DELETE', '/staff/membership/'.$staff_record->id, null, null, 204);
+
     }
 
 
