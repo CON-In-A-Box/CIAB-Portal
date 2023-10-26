67be1143fc7b50e34bcdc942cd196cf68e79fd9a
rkaiser-dev <rkaiser-dev@users.noreply.github.com>
2023-10-25 22:53:35 -0500

    Add ConCom donut functionality to Vue

--- modules/concom/sitesupport/division-parser.js
+++ modules/concom/sitesupport/division-parser.js
@@ -38,6 +38,15 @@ const extractDivisions = (departmentData) => {
     }, {});
 }
 
+const remapItem = (item, idx, total) => {
+  return {
+    label: item.name,
+    colorIndex: idx,
+    link: `index.php?Function=concom#table_header_${item.name.replaceAll(' ', '_')}`,
+    dsize: (1 / total)
+  }
+};
+
 /**
  *
  * Data comes back in format as an array:
@@ -102,4 +111,89 @@ export const extractDivisionHierarchy = (departmentData) => {
   });
 
   return sortedDivisions;
+}
+
+/**
+ * Takes the sorted division data and creates the necessary structure to render our organizational hierarchy donut.
+ *
+ * Data is returned in the following format:
+ *
+ * {
+ *   "nodeData": {
+ *     "label": "Activities",
+ *     "colorIndex": 0,
+ *     "link": "index.php?function=concom#Activities",
+ *     "dsize": 1,
+ *     "strokeWidth": 3
+ *   },
+ *   "subData": [
+ *     {
+ *       "nodeData": {
+ *         "label": "arrow":,
+ *         "colorIndex": 0,
+ *         "strokeWidth": 2,
+ *         "dsize": 1,
+ *         "noRotate": 1,
+ *         "pieWidth": 10
+ *       },
+ *       "subData": [
+ *          {
+ *            "nodeData": {
+ *              "label": "Book Swap",
+ *              "colorIndex": 0,
+ *              "link": "index.php?function=concom#Book_Swap",
+ *              "dsize": 0.125,
+ *              "strokeWidth": 1
+ *            }
+ *          }
+ *       ]
+ *     }
+ *   ]
+ * }
+ *
+ * "label" is the name of the division or department, except for the arrow
+ * "colorIndex" is the color index to use when rendering, departments should match with their division
+ * "link" is the link on the ConCom List page to navigate to a section
+ * "dsize" is the area allocated to a rendered division or department. If there are 8 departments,
+ *   then each department should have a value equal to 1/8
+ * "strokeWidth" is the line width on the rendered section area
+ * @param {*} divisions
+ * @returns
+ */
+export const createDonutData = (divisions) => {
+  return divisions.map((division, idx) => {
+    const mappedDivision = {
+      // Special Case for Divisions: dsize always 1
+      ...remapItem(division, idx, 1),
+      strokeWidth: 3
+    };
+
+    // If for some reason we have additional staff in the "department" that is really the division, we don't want to double up.
+    const filteredDivisionDepts = division.departments.filter((department) => department.id !== division.id);
+    const mappedDepartments = filteredDivisionDepts.map((department) => {
+      return {
+        nodeData: {
+          ...remapItem(department, idx, filteredDivisionDepts.length),
+          strokeWidth: 1
+        }
+      }
+    });
+
+    return {
+      nodeData: mappedDivision,
+      subData: [
+        {
+          nodeData: {
+            label: '\u{233E}',
+            colorIndex: idx,
+            strokeWidth: 2,
+            dsize: 1,
+            noRotate: 1,
+            pieWidth: 10
+          },
+          subData: mappedDepartments
+        }
+      ]
+    }
+  });
 }
--- test/sitesupport/modules/staff/__tests__/division-parser.test.js
+++ test/sitesupport/modules/staff/__tests__/division-parser.test.js
@@ -1,10 +1,42 @@
 /* globals describe, it, expect */
-import { extractDivisionHierarchy } from '../../../../../modules/concom/sitesupport/division-parser';
+import { extractDivisionHierarchy, createDonutData } from '../../../../../modules/concom/sitesupport/division-parser';
 import departmentList from './department_list.json';
 
 describe('Staff Division Parser', () => {
   it('can parse the division hierarchy', () => {
     const result = extractDivisionHierarchy(departmentList.data);
     expect(result).toMatchSnapshot();
+  });
+
+  it('can parse donut data from the division data', () => {
+    const divisions = extractDivisionHierarchy(departmentList.data);
+    const result = createDonutData(divisions);
+
+    result.forEach((item, idx) => {
+      // Division at top level node data.
+      expect(item.nodeData.colorIndex).toEqual(idx);
+      expect(item.nodeData.dsize).toEqual(1);
+      expect(item.nodeData.strokeWidth).toEqual(3);
+      expect(item.nodeData).toHaveProperty('label');
+      expect(item.nodeData.link.includes(item.nodeData.label.replaceAll(' ', '_'))).toEqual(true);
+
+      const arrowNode = item.subData[0];
+      // First level of "subData" always has one element. An arrow and then departments.
+      expect(arrowNode.nodeData.colorIndex).toEqual(idx);
+      expect(arrowNode.nodeData.dsize).toEqual(1);
+      expect(arrowNode.nodeData.noRotate).toEqual(1);
+      expect(arrowNode.nodeData.pieWidth).toEqual(10);
+      expect(arrowNode.nodeData.strokeWidth).toEqual(2);
+      expect(arrowNode.nodeData.label).toEqual('⌾');
+
+      // Departments
+      arrowNode.subData.forEach((dept) => {
+        expect(dept.nodeData.colorIndex).toEqual(idx);
+        expect(dept.nodeData.dsize).toBeCloseTo((1 / arrowNode.subData.length), 5);
+        expect(dept.nodeData.strokeWidth).toEqual(1);
+        expect(dept.nodeData).toHaveProperty('label');
+        expect(dept.nodeData.link.includes(dept.nodeData.label.replaceAll(' ', '_'))).toEqual(true);
+      });
+    });
   });
 });
